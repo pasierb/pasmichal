ad01f8777a0eafd7e56333c24c1d323ba103f24e
Michal Pasierbski <mpasierbski@gmail.com>
2025-05-14 20:51:22 +0200

    Add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,16 @@ import { defineConfig } from "astro/config";
 import remarkToc from 'remark-toc';
 import tailwind from "@astrojs/tailwind";
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
+  integrations: [tailwind()],
+
   markdown: {
     remarkPlugins: [ [remarkToc, { heading: 'On this page', maxDepth: 3 } ] ],
   },
+
   site: 'https://www.pasmichal.com',
+  adapter: netlify(),
 });
